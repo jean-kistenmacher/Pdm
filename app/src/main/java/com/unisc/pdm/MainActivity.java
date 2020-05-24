128a7c7c8af148d4aa1214df0cffb7c4ef5384c4
ghelfer <ghelfer@gmail.com>
2020-05-24 01:27:48 -0300

    trabalho

--- app/src/main/java/com/unisc/pdm/MainActivity.java
+++ app/src/main/java/com/unisc/pdm/MainActivity.java
@@ -1,16 +1,25 @@
 package com.unisc.pdm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
@@ -47,7 +56,56 @@ public class MainActivity extends Activity {
         startActivity(new Intent(this,Aula8Activity.class));
     }
 
-    public void aula9Click(View view) { startActivity(new Intent(this,Aula9Activity.class));
+    public void aula9Click(View view) { startActivity(new Intent(this,Aula9Activity.class));  }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                } else {
+                    runCameraActivity();
+                }
+                return true;
+        }
+        return false;
+    }
+
+    private void runCameraActivity() {
+        Intent intent = new Intent(this, TrabalhoActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0) {
+            if (requestCode == 1) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    MessageBox("Camera permission granted!");
+                    runCameraActivity();
+                } else {
+                    MessageBox("Camera permission denied!");
+                }
+            }
+        } else {
+            MessageBox("Camera permission denied!");
+        }
+    }
+
+    void MessageBox(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
 }
